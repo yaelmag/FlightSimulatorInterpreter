45ebbe7d883670a3a7467d9f5d80496213772842
yaelmag <yael11072@gmail.com>
2018-12-25 18:56:05 +0200

    Merge branch 'master' of https://github.com/yaelmag/project1
    # Conflicts:
    #	CMakeLists.txt

--- Client.cpp
+++ Client.cpp
@@ -2,4 +2,60 @@
 // Created by yael on 12/24/18.
 //
 
+#include <netinet/in.h>
+#include <netdb.h>
 #include "Client.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#include <string.h>
+
+Client::Client(int port, char* ipByName) {
+    this->port = port;
+    this->ipByName = ipByName;
+}
+
+void Client::writeData(string massege, double value) {
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    string buffer = "set " + massege + " " + to_string(value) + "\r\n";
+
+/* Create a socket point */
+    this->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (this->sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    server = gethostbyname(ipByName);
+
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(this->port);
+
+/* Now connect to the server */
+    if (connect(this->sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR connecting");
+        exit(1);
+    }
+
+/* Send message to the server */
+    if (send(this->sockfd, buffer.c_str(), strlen(buffer.c_str()), 0) < 0) {
+        perror("send failed: ");
+        exit(1);
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,78 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <netdb.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <netinet/in.h>
-
-#include <string.h>
-
-int main(int argc, char *argv[]) {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-
-    portno = 5402;
-
-    /* Create a socket point */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0) {
-        perror("ERROR opening socket");
-        exit(1);
-    }
-
-    server = gethostbyname("10.0.2.2");
-
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-    /* Now connect to the server */
-    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR connecting");
-        exit(1);
-    }
-
-    /* Now ask for a message from the user, this message
-       * will be read by server
-    */
-
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-
-    /* Send message to the server */
-    n = write(sockfd, buffer, strlen(buffer));
-    n = write(sockfd, "\r\n", 2);
-
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
-
-    /* Now read server response */
-    bzero(buffer,256);
-    n = read(sockfd, buffer, 255);
-
-    if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-
-    printf("%s\n",buffer);
-    return 0;
-}
-/*
 #include <unistd.h>
 #include "SymbolTable.h"
 #include "VarBindMap.h"
@@ -80,20 +5,22 @@ int main(int argc, char *argv[]) {
 #include "iostream"
 #include "BooleanExpression.h"
 #include "Greater.h"
+#include "Client.h"
 
 int main() {
-    SymbolTable symbolTable;
-    symbolTable.getMap().insert(pair<string, double>("x", 5.0));
-    VarBindMap varBindMap;
-    varBindMap.getMap().insert(pair<string, string>("x", "/instrumentation/airspeed-indicator/indicated-speed-kt"));
-    int i;
-    vector<string> vec;
-    vec.push_back("5400");
-    vec.push_back("10");
-    vec.push_back("\n");
-    OpenServerCommand openServerCommand = OpenServerCommand(symbolTable, varBindMap);
-    openServerCommand.doCommand(vec, 0);
-    //cout << i << endl;
-    sleep(60);
-    return 0;
-}*/
+    Client(5402, "10.0.2.2").writeData("controls/flight/rudder", 1);
+//    SymbolTable symbolTable;
+//    symbolTable.getMap().insert(pair<string, double>("x", 5.0));
+//    VarBindMap varBindMap;
+//    varBindMap.getMap().insert(pair<string, string>("x", "/instrumentation/airspeed-indicator/indicated-speed-kt"));
+//    int i;
+//    vector<string> vec;
+//    vec.push_back("5400");
+//    vec.push_back("10");
+//    vec.push_back("\n");
+//    OpenServerCommand openServerCommand = OpenServerCommand(symbolTable, varBindMap);
+//    openServerCommand.doCommand(vec, 0);
+//    //cout << i << endl;
+//    sleep(60);
+//    return 0;
+}
